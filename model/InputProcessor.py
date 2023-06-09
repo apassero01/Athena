0446dce0f7016bc73224161cea41718ae020f72a
Andrew Passero <ajp031@bucknell.edu>
2023-06-08 21:01:56 -0400

    added webparser to generate content from tweet and website url

--- model/InputProcessor.py
+++ model/InputProcessor.py
@@ -3,13 +3,61 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from Screenshot import Screenshot
 import requests
+from selenium.webdriver.common.by import By
+from time import sleep
 
 class InputProcessor: 
     def __init__(self, url, userTags = ""): 
         self.url = url 
         self.userTags = userTags
+        self.allContent = ""
     
-    # def parseURL(self):
+    def parseURL(self):
+        driver = webdriver.Chrome()
+        driver.get(url)
+        sleep(3)
+
+        if "twitter" in url: 
+            elements = driver.find_elements(By.XPATH, "//div[@data-testid='tweetText']")
+        else:
+            allTags = ["p", "h", "title"]
+            elements = [] 
+            for tag in allTags: 
+                elements += driver.find_elements(By.TAG_NAME,tag)
+    
+        for e in elements:
+            self.allContent = self.allContent + e.text + "\n"
+
+        driver.close()
+        driver.quit()
+
+
+        #Add try and catch statements to determine if webscrape was successful    
+        isSuccess = len(self.allContent) > 0
+        return isSuccess ,self.allContent 
+
+    def generateTextFile(self): 
+        allText = self.userTags + "\n" + self.allContent
+
+        filename = "textData.txt" 
+        textFile = open(filename, 'w')
+        textFile.write(allText)
+        textFile.close()
+        return filename, self.url
+
+    
+
+
+
+url = "https://www.allrecipes.com/article/how-to-cook-steak/"
+# url = "https://twitter.com/greg_price11/status/1665792769472823299"
+processor = InputProcessor(url)
+processor.parseURL()
+processor.generateTextFile()
+
+
+#Regular website idea
+# def parseURL(self):
     #     response = requests.get(self.url) 
     #     html_content = response.content 
     #     soup = BeautifulSoup(html_content, 'html.parser')
@@ -25,27 +73,6 @@ class InputProcessor:
     #         allContent += curText
         
     #     print(allContent)
-
-    def parseURL(self):
-
-        chrome_options = Options() 
-        # chrome_options.add_argument("--headless")
-        
-        
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(self.url)
-        ob = Screenshot.Screenshot()
-        img_url = ob.full_screenshot(driver, save_path=r'.', image_name='myimage.png', is_load_at_runtime=True,
-                                          load_wait_time=3)
-        driver.close()
-        driver.quit()
-
-
-url = "https://www.allrecipes.com/article/how-to-cook-steak/"
-# url = "https://twitter.com/greg_price11/status/1665792769472823299"
-processor = InputProcessor(url)
-processor.parseURL()
-
     
     
 
